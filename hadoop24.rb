d4f06535ed393707b573ae97ee6c8bec6c71c734
John Ferguson <aninvisiblefriend.tech@gmail.com>
2015-01-09 22:30:29 -0500

    First commit.  Minimally verified.

---
+++ hadoop24.rb
@@ -0,0 +1,35 @@
+class Hadoop24 < Formula
+
+  homepage "http://hadoop.apache.org/"
+  url "https://archive.apache.org/dist/hadoop/core/hadoop-2.4.1//hadoop-2.4.1.tar.gz"
+  sha1 "bec3d911fd0b84cd4a7130fb212125c7ae51e878"
+
+  def install
+    rm_f Dir["bin/*.cmd", "sbin/*.cmd", "libexec/*.cmd", "etc/hadoop/*.cmd"]
+    libexec.install %w[bin sbin libexec share etc]
+    bin.write_exec_script Dir["#{libexec}/bin/*"]
+    sbin.write_exec_script Dir["#{libexec}/sbin/*"]
+    # But don't make rcc visible, it conflicts with Qt
+    (bin/"rcc").unlink
+
+    inreplace "#{libexec}/etc/hadoop/hadoop-env.sh",
+      "export JAVA_HOME=${JAVA_HOME}",
+      "export JAVA_HOME=\"$(/usr/libexec/java_home)\""
+    inreplace "#{libexec}/etc/hadoop/yarn-env.sh",
+      "# export JAVA_HOME=/home/y/libexec/jdk1.6.0/",
+      "export JAVA_HOME=\"$(/usr/libexec/java_home)\""
+    inreplace "#{libexec}/etc/hadoop/mapred-env.sh",
+      "# export JAVA_HOME=/home/y/libexec/jdk1.6.0/",
+      "export JAVA_HOME=\"$(/usr/libexec/java_home)\""
+  end
+
+  def caveats; <<-EOS.undent
+    In Hadoop's config file:
+      #{libexec}/etc/hadoop/hadoop-env.sh,
+      #{libexec}/etc/hadoop/mapred-env.sh and
+      #{libexec}/etc/hadoop/yarn-env.sh
+    $JAVA_HOME has been set to be the output of:
+      /usr/libexec/java_home
+    EOS
+  end
+end
